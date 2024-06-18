2302acf68173d1dd5aaa73e0d18f047c14d3229d
chunchen0118 <138970546+chunchen0118@users.noreply.github.com>
2024-06-18 11:22:25 +0800

    Add files via upload

--- audioModule/volumeProcessor.js
+++ audioModule/volumeProcessor.js
@@ -2,16 +2,15 @@ class VolumeProcessor extends AudioWorkletProcessor {
     constructor() {
         super();
         this.volume = 0;
-        // console.log('VolumeProcessor 載入成功');
     }
-
+    
     process(inputs, outputs, parameters) {
         const input = inputs[0];
         if (input.length > 0) {
             const channelData = input[0];
             let sum = 0;
             for (let i = 0; i < channelData.length; i++) {
-            sum += channelData[i] * channelData[i];
+                sum += channelData[i] * channelData[i];
             }
             this.volume = Math.sqrt(sum / channelData.length);
             this.port.postMessage(this.volume);
